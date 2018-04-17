bb26d44b1b04ba777049ec7f62f070e8ff2df5c5
dgieedelman <dgieedelman@gmail.com>
2018-04-17 14:02:16 -0400

    FILLER

--- examples/example_sender.jl
+++ examples/example_sender.jl
@@ -1,15 +1,21 @@
 using LCMCore
 using CaesarLCMTypes
 
-
+lcm=LCM()
 function generate_example_msg()
   msg = example_t()
   msg.timestamp = 0
   msg.position = (1, 2, 3)
   msg.orientation = (1, 0, 0, 0)
-  msg.ranges = range(15)
-  msg.num_ranges = len(msg.ranges)
+  msg.ranges = randn(15)
+  msg.num_ranges = length(msg.ranges)
   msg.name = "example string"
-  msg.enabled = True
+  msg.enabled = true
   return msg
 end
+
+msg = generate_example_msg()
+
+data=encode(msg)
+
+publish(lcm,"TEST_CHANNEL",data)
